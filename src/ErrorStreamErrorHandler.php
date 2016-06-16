bbb02871ce4c130f07698ddeb9d1a67b4d9236ad
Kevin Hamilton <kevinh448@gmail.com>
2016-06-15 18:45:00 -0700

    dev test

--- src/ErrorStreamErrorHandler.php
+++ src/ErrorStreamErrorHandler.php
@@ -1,7 +1,71 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: kevin
- * Date: 6/15/16
- * Time: 6:32 PM
- */
+
+namespace ErrorStream\ErrorStream;
+
+use Yii;
+use yii\base\InvalidConfigException;
+use yii\web\Application;
+use ErrorStream\ErrorStreamClient\ErrorStreamClient;
+use ErrorStream\ErrorStreamClient\ErrorStreamReport;
+
+
+class ErrorStreamErrorHandler extends \yii\web\ErrorHandler
+{
+    
+    public $clientId = 'errorstream';
+    
+    
+    public function init()
+    {
+        Yii::$app->on(Application::EVENT_BEFORE_REQUEST, [$this, 'onShutdown']);
+    }
+
+    public function onShutdown()
+    {
+        $error = error_get_last();
+        if ($error !== null) {
+
+            $errors = array(
+                E_ERROR,
+                E_PARSE,
+                E_CORE_ERROR,
+                E_CORE_WARNING,
+                E_COMPILE_ERROR,
+                E_COMPILE_WARNING,
+                E_STRICT
+            );
+
+            if (in_array($error['type'], $errors)) {
+                $this->getClient()->captureException($this->createErrorException($error['message'], $error['type'], $error['file'], $error['line']));
+            }
+        }
+    }
+
+    public function handleError($code, $message, $file, $line)
+    {
+        if (error_reporting() & $code) {
+            $this->getClient()->captureException($this->createErrorException($message, $code, $file, $line));
+        }
+
+        parent::handleError($code, $message, $file, $line);
+    }
+
+    public function handleException($exception)
+    {
+        $this->getClient()->reportException($exception);
+        parent::handleException($exception);
+    }
+
+    protected function createErrorException($message, $code, $file, $line)
+    {
+        return new \ErrorException($message, $code, 0/* will be resolved */, $file, $line);
+    }
+
+    public function getClient()
+    {
+        if (!Yii::$app->has($this->clientId)) {
+            throw new InvalidConfigException(sprintf('ErrorStream.componentID "%s" is invalid.', $this->clientId));
+        }
+        return Yii::$app->get($this->clientId);
+    }
+}
--- src/ErrorStreamLogger.php
+++ src/ErrorStreamLogger.php
@@ -1,7 +1,81 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: kevin
- * Date: 6/15/16
- * Time: 6:31 PM
- */
+
+namespace ErrorStream\ErrorStream;
+
+use Yii;
+use yii\base\InvalidConfigException;
+use yii\log\Logger;
+use yii\log\Target;
+use ErrorStream\ErrorStreamClient\ErrorStreamClient;
+use ErrorStream\ErrorStreamClient\ErrorStreamReport;
+
+
+class ErrorStreamLogger extends Target
+{
+
+    public $clientId = 'errorstream';
+
+    /**
+     *  0 => message
+     *  1 => level
+     *  2 => category
+     *  3 => timestamp
+     */
+    protected function processLogs($logs)
+    {
+        foreach ($logs as $log) {
+            $array = explode("\n", $log[0]);
+            $message = implode('<br>', $array);
+
+
+            $report = new ErrorStreamReport();
+            $report->error_group = $message;
+            $report->line_number = 0;
+            $report->file_name = 'N/A';
+            $report->message = $message;
+            $report->stack_trace = $message;
+            $report->severity =  $this->getSeverity($log);
+
+            
+            $this->getClient()->reportException($report);
+        }
+    }
+
+    protected function getSeverity($log){
+        $severity = 3;
+        $name = Logger::getLevelName($log[1]);
+        if($name == 'trace') $severity = 1;
+        if($name == 'info') $severity = 1;
+        if($name == 'profile') $severity = 2;
+        if($name == 'warning') $severity = 2;
+        if($name == 'error') $severity = 3;
+        return $severity;
+    }
+
+    public function export()
+    {
+        $client = $this->getClient();
+        foreach($this->messages as $message) {
+
+            list($text, $level, $category, $timestamp) = $message;
+
+            $report = new ErrorStreamReport();
+            $report->error_group = $message;
+            $report->line_number = 0;
+            $report->file_name = 'N/A';
+            $report->message = $text;
+            $report->stack_trace = $text;
+            $report->severity =  $this->getSeverity($level);
+
+            $client->reportException($report);
+        }
+    }
+
+    public function getClient()
+    {
+        if (!Yii::$app->has($this->clientId)) {
+            throw new InvalidConfigException(sprintf('ErrorStream.componentID "%s" is invalid.', $this->clientId));
+        }
+        return Yii::$app->get($this->clientId);
+    }
+}
